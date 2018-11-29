627513166db3bd0dd29bdf4e4e8a9deca09e1055
Opelkuh <opelkja15@sps-prosek.cz>
2018-11-29 15:41:17 +0100

    Add fields to order entity

--- src/orders/order.entity.ts
+++ src/orders/order.entity.ts
@@ -1,4 +1,11 @@
-import { Entity, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  ManyToMany,
+  JoinTable,
+  Column,
+} from 'typeorm';
 import { Customer } from 'customers/customer.entity';
 import { Item } from 'items/item.entity';
 
@@ -6,6 +13,12 @@ import { Item } from 'items/item.entity';
 export class Order {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ default: false })
+  paid: boolean;
+
+  @Column({ default: false })
+  shipped: boolean;
 
   @ManyToMany(type => Item, item => item.orders)
   @JoinTable()
